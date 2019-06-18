5926d14a7edb09743f67d946b2462bf3feba302a
xi <2528190767@qq.com>
2019-06-18 08:37:41 +0800

    把本地工程提交到GIT

---
+++ maven_high_git01/src/com/it/git/FirstGit.java
@@ -0,0 +1,12 @@
+package com.it.git;
+
+public class FirstGit {
+    public static void main(String[] args) {
+        System.out.println("创建第一个Git");
+        System.out.println("创建第一个Git");
+        System.out.println("创建第一个Git");
+        System.out.println("创建第一个Git");
+        System.out.println("创建第一个Git");
+        System.out.println("创建第一个Git");
+    }
+}
